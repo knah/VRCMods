8f0071062b771787a20b93dc258dabb6c16f4eac
Nikolay Kuznetsov <kna.rus@gmail.com>
2020-05-15 18:49:39 +0200

    Update MirrorResolution for latest VRC, add MSAA setting

--- MirrorResolutionUnlimiter/MirrorResolutionUnlimiterMod.cs
+++ MirrorResolutionUnlimiter/MirrorResolutionUnlimiterMod.cs
@@ -7,7 +7,6 @@ using UnhollowerBaseLib;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using VRC.SDKBase;
-using Object = UnityEngine.Object;
 
 [assembly:MelonModInfo(typeof(MirrorResolutionUnlimiterMod), "MirrorResolutionUnlimiter", "1.0", "knah")]
 [assembly:MelonModGame("VRChat", "VRChat")]
@@ -19,12 +18,18 @@ namespace MirrorResolutionUnlimiter
         private const string ModCategory = "MirrorResolutionUnlimiter";
         
         private const string MaxResPref = "MaxEyeTextureResolution";
+        private const string MirrorMsaaPref = "MirrorMsaa";
         private const string AllMirrorsAutoPref = "AllMirrorsUseAutoRes";
+
+        private static int ourMaxEyeResolution = 2048;
+        private static bool ourAllMirrorsAuto = false;
+        private static int ourMirrorMsaa = 0;
 
         public override void OnApplicationStart()
         {
             ModPrefs.RegisterCategory(ModCategory, "Mirror Resolution");
             ModPrefs.RegisterPrefInt(ModCategory, MaxResPref, 4096, "Max eye texture size");
+            ModPrefs.RegisterPrefInt(ModCategory, MirrorMsaaPref, 0, "Mirror MSAA (0=default)");
             ModPrefs.RegisterPrefBool(ModCategory, AllMirrorsAutoPref, false, "Force auto resolution");
             
             unsafe
@@ -34,60 +39,59 @@ namespace MirrorResolutionUnlimiter
             }
             
             SceneManager.add_sceneLoaded(new Action<Scene, LoadSceneMode>((_, __) => OnModSettingsApplied()));
+            
+            OnModSettingsApplied();
         }
 
         public override void OnModSettingsApplied()
         {
-            if (!ModPrefs.GetBool(ModCategory, AllMirrorsAutoPref)) return;
-            
-            foreach (var vrcMirrorReflection in Object.FindObjectsOfType<VRC_MirrorReflection>())
-                vrcMirrorReflection.mirrorResolution = VRC_MirrorReflection.Dimension.Auto;
+            ourMaxEyeResolution = ModPrefs.GetInt(ModCategory, MaxResPref);
+            ourAllMirrorsAuto = ModPrefs.GetBool(ModCategory, AllMirrorsAutoPref);
+            ourMirrorMsaa = ModPrefs.GetInt(ModCategory, MirrorMsaaPref);
+            if (ourMirrorMsaa != 1 && ourMirrorMsaa != 2 && ourMirrorMsaa != 4 && ourMirrorMsaa != 8)
+                ourMirrorMsaa = 0;
         }
 
+        // this is slightly rewritten code from VRCSDK
+        // if only the game was still in IL so that transpiler harmony patches worked
         private static IntPtr GetReflectionData(IntPtr thisPtr, IntPtr currentCameraPtr)
         {
             var @this = new VRC_MirrorReflection(thisPtr);
             var currentCamera = new Camera(currentCameraPtr);
 
-            VRC_MirrorReflection.ReflectionData reflectionData;
-            if (@this._mReflections == null)
-                @this._mReflections = new Dictionary<Camera, VRC_MirrorReflection.ReflectionData>();
+            var reflections = @this._mReflections ?? (@this._mReflections = new Dictionary<Camera, VRC_MirrorReflection.ReflectionData>());
 
-            if (!@this._mReflections.ContainsKey(currentCamera))
-            {
-                reflectionData = new VRC_MirrorReflection.ReflectionData()
-                {
-                    propertyBlock = new MaterialPropertyBlock()
-                };
-                @this._mReflections[currentCamera] = reflectionData;
-            }
-            else
-            {
-                reflectionData = @this._mReflections[currentCamera];
-            }
+            // TryGetValue crashes in unhollower 0.4
+            var reflectionData = reflections.ContainsKey(currentCamera)
+                ? reflections[currentCamera]
+                : reflections[currentCamera] = new VRC_MirrorReflection.ReflectionData {propertyBlock = new MaterialPropertyBlock()};
+
+            if (@this._temporaryRenderTexture)
+                RenderTexture.ReleaseTemporary(@this._temporaryRenderTexture);
+            if (reflectionData.texture[0])
+                RenderTexture.ReleaseTemporary(reflectionData.texture[0]);
+            if (reflectionData.texture[1])
+                RenderTexture.ReleaseTemporary(reflectionData.texture[1]);
             
             int width;
             int height;
-            var maxEyeSize = ModPrefs.GetInt(ModCategory, MaxResPref);
 
-            if (@this.mirrorResolution == VRC_MirrorReflection.Dimension.Auto || ModPrefs.GetBool(ModCategory, AllMirrorsAutoPref))
+            if (ourAllMirrorsAuto || @this.mirrorResolution == VRC_MirrorReflection.Dimension.Auto)
             {
-                width = Mathf.Min(currentCamera.pixelWidth, maxEyeSize);
-                height = Mathf.Min(currentCamera.pixelHeight, maxEyeSize);
+                width = Mathf.Min(currentCamera.pixelWidth, ourMaxEyeResolution);
+                height = Mathf.Min(currentCamera.pixelHeight, ourMaxEyeResolution);
             }
             else
-            {
-                width = (int) @this.mirrorResolution;
-                height = (int) @this.mirrorResolution;
-            }
+                width = height = (int) @this.mirrorResolution;
 
-            int antiAliasing = Mathf.Max(1, QualitySettings.antiAliasing);
-            for (int index = 0; index < 2 && (index <= 0 || currentCamera.stereoEnabled); ++index)
+            int antiAliasing = ourMirrorMsaa == 0 ? Mathf.Clamp(1, QualitySettings.antiAliasing, (int) @this.maximumAntialiasing) : ourMirrorMsaa;
+            @this._temporaryRenderTexture = RenderTexture.GetTemporary(width, height, 24, RenderTextureFormat.ARGBHalf, RenderTextureReadWrite.Default, antiAliasing);
+            reflectionData.texture[0] = RenderTexture.GetTemporary(width, height, 24, RenderTextureFormat.ARGBHalf, RenderTextureReadWrite.Default, 1);
+            reflectionData.propertyBlock.SetTexture(VRC_MirrorReflection._texturePropertyId[0], reflectionData.texture[0]);
+            if (currentCamera.stereoEnabled)
             {
-                if (reflectionData.texture[index])
-                    RenderTexture.ReleaseTemporary(reflectionData.texture[index]);
-                reflectionData.texture[index] = RenderTexture.GetTemporary(width, height, 24, RenderTextureFormat.ARGBHalf, RenderTextureReadWrite.Default, antiAliasing);
-                reflectionData.propertyBlock.SetTexture(VRC_MirrorReflection._texturePropertyId[index], reflectionData.texture[index]);
+                reflectionData.texture[1] = RenderTexture.GetTemporary(width, height, 24, RenderTextureFormat.ARGBHalf, RenderTextureReadWrite.Default, 1);
+                reflectionData.propertyBlock.SetTexture(VRC_MirrorReflection._texturePropertyId[1], reflectionData.texture[1]);
             }
 
             return reflectionData.Pointer;
