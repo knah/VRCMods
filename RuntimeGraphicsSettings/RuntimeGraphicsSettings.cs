ef59618fbbe7881859207d8c185edef13a97cd6d
Nikolay Kuznetsov <kna.rus@gmail.com>
2020-05-27 21:47:25 +0200

    Migrate mods to netcore-style SDK projects

--- RuntimeGraphicsSettings/RuntimeGraphicsSettings.cs
+++ RuntimeGraphicsSettings/RuntimeGraphicsSettings.cs
@@ -19,14 +19,14 @@ namespace RuntimeGraphicsSettings
         {
             ModPrefs.RegisterCategory(CategoryName, "Graphics settings");
             
-            ModPrefs.RegisterPrefInt(CategoryName, MsaaLevel, -1);
-            ModPrefs.RegisterPrefBool(CategoryName, AllowMsaa, true);
-            ModPrefs.RegisterPrefBool(CategoryName, AnisoFilter, true);
-            ModPrefs.RegisterPrefBool(CategoryName, RealtimeShadows, true);
-            ModPrefs.RegisterPrefBool(CategoryName, SoftShadows, true);
-            ModPrefs.RegisterPrefInt(CategoryName, PixelLights, -1);
+            ModPrefs.RegisterPrefInt(CategoryName, MsaaLevel, -1, "MSAA Level (1/2/4/8)");
+            ModPrefs.RegisterPrefBool(CategoryName, AllowMsaa, true, "Enable MSAA");
+            ModPrefs.RegisterPrefBool(CategoryName, AnisoFilter, true, "Enable anisotropic filtering");
+            ModPrefs.RegisterPrefBool(CategoryName, RealtimeShadows, true, "Realtime shadows");
+            ModPrefs.RegisterPrefBool(CategoryName, SoftShadows, true, "Soft shadows");
+            ModPrefs.RegisterPrefInt(CategoryName, PixelLights, -1, "Max pixel lights");
             ModPrefs.RegisterPrefInt(CategoryName, TextureLimit, -1, "Texture decimation");
-            ModPrefs.RegisterPrefInt(CategoryName, GraphicsTier, -1);
+            ModPrefs.RegisterPrefInt(CategoryName, GraphicsTier, -1, "Graphics tier (1/2/3)");
         }
 
         public static bool AllowMSAA => ModPrefs.GetBool(CategoryName, AllowMsaa);
