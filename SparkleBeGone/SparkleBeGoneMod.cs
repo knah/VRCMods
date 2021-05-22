d473e2ad702e1926913b3f3a7137c08bbc35342c
Nikolay Kuznetsov <kna.rus@gmail.com>
2021-05-22 18:37:49 +0200

    SBG: update obsolete method usages, optimize a bit, apply settings immediately when changed

--- SparkleBeGone/SparkleBeGoneMod.cs
+++ SparkleBeGone/SparkleBeGoneMod.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections;
 using System.IO;
-using System.Linq;
 using System.Reflection;
 using System.Runtime.InteropServices;
 using MelonLoader;
@@ -11,23 +10,12 @@ using UnhollowerRuntimeLib;
 using UnityEngine;
 
 [assembly:MelonGame("VRChat", "VRChat")]
-[assembly:MelonInfo(typeof(SparkleBeGoneMod), "SparkleBeGone", "1.0.2", "knah", "https://github.com/knah/VRCMods")]
+[assembly:MelonInfo(typeof(SparkleBeGoneMod), "SparkleBeGone", "1.1.0", "knah", "https://github.com/knah/VRCMods")]
 
 namespace SparkleBeGone
 {
     public class SparkleBeGoneMod : CustomizedMelonMod
     {
-        private const string SettingsCategory = "SparkleBeGone";
-        
-        private const string StartSparkleSetting = "StartSparkle";
-        private const string EndSparksSetting = "EndSparks";
-        private const string EndFlareSetting = "EndFlare";
-        
-        private const string RecolorSparksSetting = "RecolorSparks";
-        private const string RecolorBeamsSetting = "RecolorBeams";
-
-        private const string BeamColorSetting = "BeamColor";
-        
         [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
         private delegate void VoidDelegate(IntPtr thisPtr);
 
@@ -39,20 +27,41 @@ namespace SparkleBeGone
         private static bool ourDoRecolorSparks;
         private static bool ourDoRecolorBeams;
         
+        private static VRCSpaceUiPointer ourLeftPointer;
+        private static VRCSpaceUiPointer ourRightPointer;
+        private static readonly int ourTintColor = Shader.PropertyToID("_TintColor");
+
+        private static MelonPreferences_Entry<bool> ourStartSparkle;
+        private static MelonPreferences_Entry<bool> ourEndSparkle;
+        private static MelonPreferences_Entry<bool> ourEndFlare;
+
         public override void OnApplicationStart()
         {
-            MelonPrefs.RegisterCategory(SettingsCategory, "Sparkle Be Gone");
+            var category = MelonPreferences.CreateCategory("SparkleBeGone", "Sparkle Be Gone");
             
-            MelonPrefs.RegisterBool(SettingsCategory, StartSparkleSetting, false, "Show start sparkle");
-            MelonPrefs.RegisterBool(SettingsCategory, EndSparksSetting, false, "Show end sparks");
-            MelonPrefs.RegisterBool(SettingsCategory, EndFlareSetting, true, "Show end flare");
+            ourStartSparkle = (MelonPreferences_Entry<bool>) category.CreateEntry("StartSparkle", false, "Show start sparkle");
+            ourEndSparkle = (MelonPreferences_Entry<bool>) category.CreateEntry("EndSparks", false, "Show end sparks");
+            ourEndFlare = (MelonPreferences_Entry<bool>) category.CreateEntry("EndFlare", true, "Show end flare");
             
-            MelonPrefs.RegisterBool(SettingsCategory, RecolorSparksSetting, false, "Recolor sparks");
-            MelonPrefs.RegisterBool(SettingsCategory, RecolorBeamsSetting, true, "Recolor beams");
+            var recolorSparks = (MelonPreferences_Entry<bool>) category.CreateEntry("RecolorSparks", false, "Recolor sparks");
+            var recolorBeams = (MelonPreferences_Entry<bool>) category.CreateEntry("RecolorBeams", true, "Recolor beams");
+            var beamColor = (MelonPreferences_Entry<string>) category.CreateEntry("BeamColor", "25 50 255 255", "Beam color (r g b a)");
             
-            MelonPrefs.RegisterString(SettingsCategory, BeamColorSetting, "25 50 255 255", "Beam color (r g b a)");
+            recolorSparks.OnValueChanged += (_, value) => ourDoRecolorSparks = value;
+            recolorBeams.OnValueChanged += (_, value) =>
+            {
+                ourDoRecolorBeams = value;
+                UpdateBeamTextures();
+            };
+            beamColor.OnValueChanged += (_, value) => ourBeamColor = DecodeColor(value);
 
-            var method = typeof(VRCSpaceUiPointer).GetMethod(nameof(VRCSpaceUiPointer.LateUpdate), BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+            ourDoRecolorSparks = recolorSparks.Value;
+            ourDoRecolorBeams = recolorBeams.Value;
+            ourBeamColor = DecodeColor(beamColor.Value);
+
+            ourStartSparkle.OnValueChanged += (_, _) => UpdateParticleSystems();
+            ourEndSparkle.OnValueChanged += (_, _) => UpdateParticleSystems();
+            ourEndFlare.OnValueChanged += (_, _) => UpdateParticleSystems();
 
             {
                 using var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("SparkleBeGone.sparklebegone");
@@ -65,18 +74,13 @@ namespace SparkleBeGone
 
             unsafe
             {
+                var method = typeof(VRCSpaceUiPointer).GetMethod(nameof(VRCSpaceUiPointer.LateUpdate), BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
                 var originalPointer = *(IntPtr*)(IntPtr) UnhollowerUtils.GetIl2CppMethodInfoPointerFieldForGeneratedMethod(method).GetValue(null);
-                CompatHook((IntPtr) (&originalPointer), typeof(SparkleBeGoneMod).GetMethod(nameof(CursorLateUpdatePatch), BindingFlags.Static | BindingFlags.NonPublic)!.MethodHandle.GetFunctionPointer());
+                MelonUtils.NativeHookAttach((IntPtr) (&originalPointer), typeof(SparkleBeGoneMod).GetMethod(nameof(CursorLateUpdatePatch), BindingFlags.Static | BindingFlags.NonPublic)!.MethodHandle.GetFunctionPointer());
                 ourOriginalLateUpdate = Marshal.GetDelegateForFunctionPointer<VoidDelegate>(originalPointer);
             }
 
             MelonCoroutines.Start(InitThings());
-        }
-        
-        private static void CompatHook(IntPtr first, IntPtr second)
-        {
-            typeof(Imports).GetMethod("Hook", BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic)!
-                .Invoke(null, new object[] {first, second});
         }
 
         private Color DecodeColor(string color)
@@ -95,48 +99,51 @@ namespace SparkleBeGone
             return new Color(red / 255f, green / 255f, blue / 255f, alpha / 255f);
         }
 
-        public override void OnModSettingsApplied()
+        private void UpdateBeamTextures()
         {
-            var cursorManager = VRCUiCursorManager.field_Private_Static_VRCUiCursorManager_0;
-
-            if (cursorManager == null) return;
-
-            var leftHand = cursorManager.transform.Find("LeftHandBeam").GetComponent<VRCUiCursor>();
-            var rightHand = cursorManager.transform.Find("RightHandBeam").GetComponent<VRCUiCursor>();
-
+            if (ourLeftPointer == null) return;
+            
             if (myOriginalLaserTexture == null)
             {
-                myOriginalLaserTexture = leftHand.GetComponent<LineRenderer>().material.mainTexture.Cast<Texture2D>();
+                myOriginalLaserTexture = ourLeftPointer.GetComponent<LineRenderer>().material.mainTexture.Cast<Texture2D>();
                 myOriginalLaserTexture.hideFlags |= HideFlags.DontUnloadUnusedAsset;
             }
 
-            var targetTexture = MelonPrefs.GetBool(SettingsCategory, RecolorBeamsSetting) ? myWhiteLaserTexture : myOriginalLaserTexture;
-            leftHand.GetComponent<LineRenderer>().material.mainTexture = targetTexture;
-            rightHand.GetComponent<LineRenderer>().material.mainTexture = targetTexture;
+            var targetTexture = ourDoRecolorBeams ? myWhiteLaserTexture : myOriginalLaserTexture;
+            ourLeftPointer.GetComponent<LineRenderer>().material.mainTexture = targetTexture;
+            ourRightPointer.GetComponent<LineRenderer>().material.mainTexture = targetTexture;
+        }
 
-            AdjustParticleSystems(leftHand.gameObject);
-            AdjustParticleSystems(rightHand.gameObject);
-
-            var color = DecodeColor(MelonPrefs.GetString(SettingsCategory, BeamColorSetting));
-            ourBeamColor = color;
-            ourDoRecolorSparks = MelonPrefs.GetBool(SettingsCategory, RecolorSparksSetting);
-            ourDoRecolorBeams = MelonPrefs.GetBool(SettingsCategory, RecolorBeamsSetting);
+        public void UpdateParticleSystems()
+        {
+            if (ourLeftPointer == null) return;
+            
+            AdjustParticleSystems(ourLeftPointer.gameObject);
+            AdjustParticleSystems(ourRightPointer.gameObject);
         }
 
         private static void CursorLateUpdatePatch(IntPtr thisPtr)
         {
             try
             {
-                var pointer = new VRCSpaceUiPointer(thisPtr);
-
                 ourOriginalLateUpdate(thisPtr);
+                
+                if (ourLeftPointer == null) return;
+
+                VRCSpaceUiPointer pointer = null;
+                if (thisPtr == ourLeftPointer.Pointer)
+                    pointer = ourLeftPointer;
+                else if (thisPtr == ourRightPointer.Pointer)
+                    pointer = ourRightPointer;
+
+                if (pointer == null) return;
 
                 if (ourDoRecolorSparks) // this affects particles
                 {
                     var materials = pointer.field_Private_ArrayOf_Material_0;
                     if (materials != null)
                         foreach (var material in materials)
-                            material.SetColor("_TintColor", ourBeamColor);
+                            material.SetColor(ourTintColor, ourBeamColor);
                 }
 
                 if (!ourDoRecolorBeams) return;
@@ -147,7 +154,7 @@ namespace SparkleBeGone
             }
             catch (Exception ex)
             {
-                MelonLogger.LogError(ex.ToString());
+                MelonLogger.Error(ex.ToString());
             }
         }
 
@@ -155,7 +162,13 @@ namespace SparkleBeGone
         {
             while (VRCUiCursorManager.field_Private_Static_VRCUiCursorManager_0 == null) yield return null;
             
-            OnModSettingsApplied();
+            var cursorManager = VRCUiCursorManager.field_Private_Static_VRCUiCursorManager_0;
+
+            ourLeftPointer = cursorManager.transform.Find("LeftHandBeam").GetComponent<VRCSpaceUiPointer>();
+            ourRightPointer = cursorManager.transform.Find("RightHandBeam").GetComponent<VRCSpaceUiPointer>();
+            
+            UpdateBeamTextures();
+            UpdateParticleSystems();
         }
 
         private void AdjustParticleSystems(GameObject cursorRoot)
@@ -164,9 +177,9 @@ namespace SparkleBeGone
             var endFlare = cursorRoot.transform.Find("plasma_beam_flare_blue");
             var endSparks = endFlare.Find("plasma_beam_spark_002");
             
-            startParticle.GetComponent<ParticleSystem>().enableEmission = MelonPrefs.GetBool(SettingsCategory, StartSparkleSetting); 
-            endFlare.GetComponent<ParticleSystem>().enableEmission = MelonPrefs.GetBool(SettingsCategory, EndFlareSetting); 
-            endSparks.GetComponent<ParticleSystem>().enableEmission = MelonPrefs.GetBool(SettingsCategory, EndSparksSetting);
+            startParticle.GetComponent<ParticleSystem>().enableEmission = ourStartSparkle.Value; 
+            endFlare.GetComponent<ParticleSystem>().enableEmission = ourEndFlare.Value; 
+            endSparks.GetComponent<ParticleSystem>().enableEmission = ourEndSparkle.Value;
         }
     }
 }
