200575a32c234b3a5f01956b9b144c8fa21d8975
Provini <71570801+Provini@users.noreply.github.com>
2022-03-01 20:47:23 -0500

    Bump ver

--- TrueShaderAntiCrash/TrueShaderAntiCrashMod.cs
+++ TrueShaderAntiCrash/TrueShaderAntiCrashMod.cs
@@ -14,7 +14,7 @@ using UIExpansionKit.API;
 using UnityEngine.SceneManagement;
 using VRC.Core;
 
-[assembly:MelonInfo(typeof(TrueShaderAntiCrashMod), "True Shader Anticrash", "1.0.6", "knah", "https://github.com/knah/VRCMods")]
+[assembly:MelonInfo(typeof(TrueShaderAntiCrashMod), "True Shader Anticrash", "1.0.7", "knah", "https://github.com/knah/VRCMods")]
 [assembly:MelonGame("VRChat", "VRChat")]
 
 namespace TrueShaderAntiCrash
